05908db96060750f96d78efa6357b9b8f6efa7d2
brorong <81868156+brorong@users.noreply.github.com>
2026-03-01 01:32:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def get_ai_advice():
         
         # ✅ 修復點 2：使用官方最穩定支援的模型名稱
         response = client.models.generate_content(
-            model='gemini-2.5-flash-lite',
+            model='gemini-3-flash-preview',
             contents=prompt
         )
         print("✅ AI 分析完成！準備回傳至前端。")
@@ -156,5 +156,6 @@ def get_ai_advice():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
 
 
